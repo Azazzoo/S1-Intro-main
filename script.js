962102d46caeec1ace8bffcbd622f7b2e8c3acd4
Azazzoo <2295082@cegepoutaouais.qc.ca>
2023-01-16 11:48:05 -0500

    Ajout du CSS, Source :Fred Ajout CodeJava

--- script.js
+++ script.js
@@ -3,15 +3,20 @@ function ajouter(){
     // Creer une balise p
     nouveauP = document.createElement("p")
     // Ecrire dans le paragraphe
-    nouveauP.innerText = item;
+    nouveauP.innerText = item + (" :");
     // Ajouter le paragraphe a la liste
     document.getElementById("maListe").append(nouveauP);
 
     // alert("L'item est bien ajouté") // Fait un Pop-up
     let prix = document.getElementById("valeur").value;
     nouveauS = document.createElement("span")
-    nouveauS.innerText =prix + ("$");
-    nouveauP.append(nouveauS)
+    nouveauS.innerText = prix + ("$");
+    nouveauP.append(nouveauS) // Mettre a la fin du p
+
+    // mettre a jour le total
+    let AncienPrix = document.getElementById("total").innerText;
+    let nouveauPrix = parseInt(AncienPrix) + parseInt(prix);
+    document.getElementById("total").innerText = nouveauPrix;
 
 
 }
